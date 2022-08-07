3e047d90b9f8f38f631cf22a72846df3577b27ce
Julio Cesar Santos <rodriguesjuliocesar145@gmail.com>
2022-08-07 12:22:43 -0300

    fix: Corrige index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router("word_list.json")
+const router = jsonServer.router("./word_list.json")
 const middlewares = jsonServer.defaults()
 
 const express = require("express");
